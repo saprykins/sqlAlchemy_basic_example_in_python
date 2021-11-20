fe07870bc5924df79508086c7a139633a547d7db
saprykins <palatatot@gmail.com>
2021-11-20 11:04:00 +0100

    gets text from file

--- __init__.py
+++ __init__.py
@@ -6,17 +6,21 @@ from sqlalchemy.orm import sessionmaker
 engine = create_engine('sqlite:///:memory:', echo=False)
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'users'
+
+class Pdf(Base):
+    __tablename__ = 'pdfs'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    nickname = Column(String)
+    author = Column(String)
+    creation_date = Column(String)
+    modification_date = Column(String)
+    creator = Column(String)
+    status = Column(String)
+    text = Column(String)
 
     def __repr__(self):
-        return "<User(name='%s', fullname='%s', nickname='%s')>" % (
-            self.name, self.fullname, self.nickname)
+        return "<Pdf(author='%s', creation_date='%s', modification_date='%s', creator='%s', status='%s', text='%s')>" % (
+            self.author, self.creation_date, self.modification_date, self.creator, self.status, self.text)
 
 # User.__table__
 
@@ -28,22 +32,52 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
-# ed_user = User(name='ed', fullname='Ed Jones', nickname='edsnickname')
+# ed_user = User(author='ed', creation_date='Ed Jones', modification_date='edsnickname')
 # session.add(ed_user)
 
+"""
+def get_doc_text_in_dictionary():
+    file_path = 'C:\_My_Files\_FA\_ETUDES\Python\py-code\sqlAlchemy_basic_example_in_python\kxlceaxpnqqvvbxs_text.txt'
+    with open(file_path) as feed:
+        text = feed.read()
+        doc_text_in_dictionary = {"text": text, }
+        return doc_text_in_dictionary
+msg_f = get_doc_text_in_dictionary()
+"""
+
+
+def get_doc_text_in_dictionary():
+    file_path = 'C:\_My_Files\_FA\_ETUDES\Python\py-code\sqlAlchemy_basic_example_in_python\carhovchadjijffq_text.txt'
+    with open(file_path) as feed:
+        text = feed.read()
+        # doc_text_in_dictionary = {"text": text, }
+        return text
+
+
+msg_f = get_doc_text_in_dictionary()
+
+"""
+msg = 'hello world'
+msg_2 = '\n'
+msg_f = msg + msg_2 + msg
+msg_f = msg_f*10
+# print(msg)
+"""
 
 session.add_all([
-    User(name='wendy', fullname='Wendy Williams', nickname='windy'),
-    User(name='mary', fullname='Mary Contrary', nickname='mary'),
-    User(name='fred', fullname='Fred Flintstone', nickname='freddy')])
+    Pdf(author='wendy', creation_date='Wendy Williams',
+        modification_date='windy', creator='me', status='ok', text='bla'),
+    Pdf(author='mary', creation_date='Mary Contrary',
+        modification_date='mary', creator='me', status='ok', text=msg_f),
+    Pdf(author='fred', creation_date='Fred Flintstone',
+        modification_date='freddy', creator='me', status='ok', text='bla')
+])
 session.commit()
 
 
-our_user = session.query(User).filter_by(name='mary').first()
-print(our_user.fullname)
+our_pdf = session.query(Pdf).filter_by(author='mary').first()
+print(our_pdf.text)
 
 
-
-# ed_user.name
-# ed_user.nickname
-
+# ed_user.author
+# ed_user.modification_date
